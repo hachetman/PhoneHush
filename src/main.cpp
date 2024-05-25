1c7896ebc2bafc62d21218ff8793361cb945704a
Christian Grumbein <christian@grumbein.de>
2024-05-25 06:22:47 +0200

    fixup clang-tidy errors, use clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
-
 #include <cstdint>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <utility>
+#include <cstdio>
 
 #include "hardware/timer.h"
 #include "pico/time.h"
 #include "tusb.h"
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
-#include "usb_descriptors.h"
 #include "hardware/clocks.h"
 #include "GC9A01A.h"
 #include "CST816S.h"
@@ -18,22 +13,26 @@
 #include "../images/in_call.h"
 #include "../images/mic_muted.h"
 #include "../images/mic.h"
-void hid_task(void);
-void cdc_task(void);
-void serial_print(const char* str);
+
+void hid_task();
+
+void cdc_task();
+
+void serial_print(const char *str);
+
 bool serialReady = false;
 GC9A01A display;
+
 void core1_entry() {
-    while (1) {
+    while (true) {
         tud_task();
     }
 }
 
 /*------------- MAIN -------------*/
-int main(void)
-{
+int main() {
     char buffer[50];
-    bool touch_happened=0;
+    bool touch_happened = false;
     CST816S_Touch touch_res;
     uint8_t gesture;
     set_sys_clock_khz(125000, true);
@@ -42,39 +41,35 @@ int main(void)
     stdio_init_all();
     multicore_launch_core1(core1_entry);
     serial_print("Initializing\r\n");
-    clock_configure(clk_peri,
-                0,
-                CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS,
-                125 * MHZ,
-                125 * MHZ);
+    clock_configure(clk_peri, 0, CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS, 125 * MHZ, 125 * MHZ);
     uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
     uint baudrate = display.init();
     CST816S touch;
-    struct GC9A01_frame frame = {{0,0},{239,239}};
+    struct GC9A01_frame frame = {{0,   0},
+                                 {239, 239}};
     display.set_frame(frame);
     touch.init(CST816S_Mode::CST816S_ALL_Mode);
 
     display.fill_display(free_image);
     display.update_display();
-    while (1)
-    {
+    while (true) {
         sleep_ms(100);
         gesture = touch.get_Gesture();
         touch_res = touch.get_Point();
         if (touch_res.interrupt == 0) {
-            touch_happened = 0;
+            touch_happened = false;
         }
         if ((touch_res.y_point < 120) & touch_res.interrupt & (touch_happened == 0)) {
             uint8_t report = 3;
             tud_hid_report(0x20, &report, 1);
             report = 1;
             tud_hid_report(0x20, &report, 1);
-            touch_happened = 1;
+            touch_happened = true;
         }
         if ((touch_res.y_point > 120) & touch_res.interrupt & (touch_happened == 0)) {
             uint8_t report = 0;
             tud_hid_report(0x20, &report, 1);
-            touch_happened = 1;
+            touch_happened = true;
         }
     }
 }
@@ -87,8 +82,8 @@ int main(void)
 // Invoked when received GET_REPORT control request
 // Application must fill buffer report's content and return its length.
 // Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
-{
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer,
+                               uint16_t reqlen) {
     // TODO not Implemented
     (void) instance;
     (void) report_id;
@@ -101,11 +96,12 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
 
 // Invoked when received SET_REPORT control request or
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
-void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
-{
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer,
+                           uint16_t bufsize) {
     (void) instance;
     char buffer2[80];
-    snprintf(buffer2, 80, "got a report, report_type: %d, report_id %d, buff %x:\r\n", report_type, report_id, buffer[0]);
+    snprintf(buffer2, 80, "got a report, report_type: %d, report_id %d, buff %x:\r\n", report_type, report_id,
+             buffer[0]);
     serial_print(buffer2);
     if ((report_id == 36) && (*buffer == 1)) {
         display.fill_rect(in_call_image, 0, 0, 240, 240);
@@ -135,26 +131,24 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
 
 // Invoked when cdc when line state changed e.g connected/disconnected
 void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
-	(void)itf;
-	(void)rts;
+    (void) itf;
+    (void) rts;
 
-	serialReady = false;
-	if (dtr) {
-		serialReady = true;
-		tud_cdc_write_str("Connected\n");
+    serialReady = false;
+    if (dtr) {
+        serialReady = true;
+        tud_cdc_write_str("Connected\n");
         tud_cdc_write_flush();
-	}
+    }
 }
 
-void cdc_task(void)
-{
+void cdc_task() {
     // connected() check for DTR bit
     // Most but not all terminal client set this when making connection
     // if ( tud_cdc_connected() )
     {
         // connected and there are data available
-        if ( tud_cdc_available() )
-        {
+        if (tud_cdc_available()) {
             // read data
             char buf[80];
             uint32_t count = tud_cdc_read(buf, sizeof(buf));
@@ -172,9 +166,9 @@ void cdc_task(void)
 }
 
 
-void serial_print(const char* str) {
-	if (serialReady) {
-		tud_cdc_write_str(str);
+void serial_print(const char *str) {
+    if (serialReady) {
+        tud_cdc_write_str(str);
         tud_cdc_write_flush();
-	}
+    }
 }
